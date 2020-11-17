bce806de3c1a9447c430f63a9febda6b636cf451
RedPlus <redplus@redplus.net>
2020-11-17 13:53:44 +0000

    Add project files.

---
+++ MachineTypeApp.Models/MachineTypes/03_MachineTypeDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MachineTypeApp.Models.MachineTypes
+{
+    public class MachineTypeDbContext : DbContext
+    {
+        // PM> Install-Package Microsoft.EntityFrameworkCore.SqlServer
+        // PM> Install-Package Microsoft.Data.SqlClient
+
+        public MachineTypeDbContext()
+        {
+
+        }
+
+        public MachineTypeDbContext(DbContextOptions<MachineTypeDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<MachineType>  MachineTypes { get; set; }
+    }
+}
